8b2994383ae755c3cc437dab696c3aea00eb3488
sobermaster9000 <awebro207@gmail.com>
2026-09-25 22:42:18 +0800

    refactor: remove max heap and expand template for min heap

--- binary_heap.cpp
+++ binary_heap.cpp
@@ -3,18 +3,44 @@ using namespace std;
 
 class MinHeap {
     private:
+    // allocate memory for 100 integers (max size of heap for now)
+    int heap[100];
+    // initialize variable to track current size of heap
+    int heapSize;
 
     public:
+    MinHeap(): heapSize(0) {}
 
-};
+    int getSize() {
+	return heapSize;
+    }
 
-class MaxHeap {
-    private:
+    // todo: implement push function
+    void push(int x) {
 
-    public:
+    }
 
+    // todo: implement pop function
+    int pop() {
+
+    }
 };
 
 int main() {
+    // tests
+    MinHeap minheap;
+    minheap.push(1);
+    minheap.push(3);
+    minheap.push(8);
+    minheap.push(7);
+    minheap.push(0);
+
+    cout << minheap.pop() << endl; // 0
+    cout << minheap.pop() << endl; // 1
+    cout << minheap.pop() << endl; // 3
+    cout << minheap.pop() << endl; // 7
+    cout << minheap.pop() << endl; // 8
+    cout << minheap.pop() << endl; // must throw error or state that you can no longer pop since heap is empty
+    
     return 0;
 }
