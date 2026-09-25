b9f345035fda9e2c7de394dfa1e614c20d62e286
sobermaster9000 <awebro207@gmail.com>
2026-09-25 08:07:10 +0800

    initial commit

---
+++ avl_tree.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class AVLTree {
+    private:
+
+    public:
+
+};
+
+int main() {
+    return 0;
+}
---
+++ binary_heap.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class BinaryHeap {
+    private:
+
+    public:
+
+};
+
+int main() {
+    return 0;
+}
